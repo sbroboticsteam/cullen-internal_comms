91560c852199d613925e89bfa19eb686621cd139
Daniel Moses <daniel.moses@stonybrook.edu>
2019-01-31 19:20:20 -0500

    Add socket builder helper method, changed node config template

--- Node.py
+++ Node.py
@@ -22,9 +22,7 @@ class Node():
         self.topics = {}
         self.initzmq()
 
-
     def stopzmq(self):
-
         """ Shuts down all zmq stuff
 
 
@@ -33,7 +31,6 @@ class Node():
         self.context.destroy()
 
     def loop(self):
-
         """ The main node code that gets executed every loop
 
         This is the method that should be overridden for the node to do stuff
@@ -42,10 +39,8 @@ class Node():
 
         print(self.id + " needs an overridden loop method")
 
-    # TODO: allow more than one model to be used
-    # TODO: look into using different protocols
+    # TODO: use new helper methods
     def initzmq(self):
-
         """This method initializes zmq sockets and places them in the topics dict
 
         It will throw exceptions if the JSON it was fed is not correct
@@ -64,15 +59,18 @@ class Node():
                         socket = self.context.socket(zmq.PUB)
                     else:
                         socket = self.context.socket(zmq.REP)
-                    socket.bind(self.configData['protocol'] + "://*:%s" % self.configData['port'])
+                    socket.bind(
+                        self.configData['protocol'] + "://*:%s" % self.configData['port'])
                     self.topics[x] = socket
                 elif y == "sub" or y == "req":
                     if y == "sub":
                         socket = self.context.socket(zmq.SUB)
                     else:
                         socket = self.context.socket(zmq.REQ)
-                    print(self.configData['protocol'] + "://localhost:%s" % self.configData['port'])
-                    socket.connect(self.configData['protocol'] + "://localhost:%s" % self.configData['port'])
+                    print(self.configData['protocol'] +
+                          "://localhost:%s" % self.configData['port'])
+                    socket.connect(
+                        self.configData['protocol'] + "://localhost:%s" % self.configData['port'])
                     if y == "sub":
                         socket.setsockopt_string(zmq.SUBSCRIBE, x)
                     self.topics[x] = socket
@@ -80,13 +78,12 @@ class Node():
                     raise Exception("Topic %s should be either pub or sub" % x)
 
     def gen_address(self, protocol, address, port):
-
         """ This method builds a url from info in a json file
 
         It can create a url for ipc, udp and tcp.
         It will throw exceptions if the protocol is invalid
         """
-        
+
         url = ""
 
         if protocol == "tcp":
@@ -97,7 +94,7 @@ class Node():
             url = "ipc://"
         else:
             raise Exception("Protocol not ipc or udp or tcp")
-        
+
         url += address
 
         if protocol == "tcp" or protocol == "udp":
@@ -105,48 +102,73 @@ class Node():
 
         return url
 
-    def build_socket(self, paradigm, url):
-        pass    
+    def build_socket(self, paradigm, topic, url):
+        """ This method creates a socket from a paradigm and a url
+
+
+        """
+
+        socket = None
+        if paradigm == "sub":
+            socket = self.context.socket(zmq.SUB)
+            socket.connect(url)
+            socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+        elif paradigm == "pub":
+            socket = self.context.socket(zmq.PUB)
+            socket.connect(url)
+        elif paradigm == "req":
+            socket = self.context.socket(zmq.REQ)
+            socket.bind(url)
+        elif paradigm == "rep":
+            socket == self.context.socket(zmq.REP)
+            socket.bind(url)
+        else:
+            raise Exception("Please provide a valid paradigm")
+
+        return socket
 
     def send(self, topic, msg):
-
         """This method can be used to send messages for the pub pattern
 
-        The first argument is the topic to send the message on and the second is the message body
+        The first argument is the topic to send the message on and the second
+        is the message body
         """
         self.topics[topic].send_string("%s %s" % (topic, msg))
 
     # TODO: implement a timeout
     def recv(self, topic, callback):
-
         """This method is used to receive messages for the sub pattern
 
         The first argument is the topic to look for messages on.
-        The second argument is a function to be executed with the message received being passed to it as an argument
-        NOT VERIFIED: This method is blocking, and will interrupt execution until a message is received
+        The second argument is a function to be executed with the message
+        received being passed to it as an argument
+        NOT VERIFIED: This method is blocking, and will interrupt execution
+        until a message is received
         """
         re = self.topics[topic].recv_string()
         callback(re)
 
     def request(self, topic, req, callback):
-
         """This method is used to send a request to a node
 
-        The first argument is the topic(in this case, the node) to send a request to.
+        The first argument is the topic(in this case, the node) to send a
+        request to.
         The second argument is the request to send
-        The third argument is a callback function to process the reply from the server. The reply will be a string
+        The third argument is a callback function to process the reply from
+        the server. The reply will be a string
         """
         self.topics[topic].send_string(req)
         msg = self.topics[topic].recv_string()
         callback(msg)
 
     def reply(self, topic, callback):
-
         """This method is used to send a reply to a node
 
         The first argument is the topic(in this case, the node) to reply to
-        The second argument is a callback that will handle the request sent to this node. It must return a string.
-        The reply generated by the callback is sent as a reply to the node that sent a request
+        The second argument is a callback that will handle the request sent to
+        this node. It must return a string.
+        The reply generated by the callback is sent as a reply to the node
+        that sent a request
         """
         msg = self.topics[topic].recv_string()
         rep = callback(msg)
