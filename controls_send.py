534e0cd880e6d42aba040aa02bfe2d89ca3224e8
keithohno <keithohno@gmail.com>
2019-02-16 18:01:40 +0000

    Now processes the arm motor controller.
    Sends an array for the power of the linear actuator, as well as for
    every servo besides the one controlling the claw. Does it work? I have
    no idea. But there really isn't any way for me to test it at the moment.

--- controls_send.py
+++ controls_send.py
@@ -5,45 +5,40 @@ from Node import Node
 import json
 import time
 
+# Basic initialization stuff for the node and joysticks
 node = Node("controls_send.json")
-
 sdl2.SDL_Init(sdl2.SDL_INIT_JOYSTICK)
-
 arm_joy = sdl2.SDL_JoystickOpen(0)
 drive_joy = sdl2.SDL_JoystickOpen(1)
 
+# Whether or not the xbox button of either controller is pressed
 pressed14 = False
 
+# Processes input from the arm controller
+def handle_arm_input(data):
+    arm_rotation = (data[5] - data[2]) // 64
+    linear_actuator = data[0] // 32
+    elbow = data[1] // 32
+    wrist_left = (data[4] + data[3]) // 32
+    wrist_right = (-data[4] - data[3]) // 32
+    return [arm_rotation, linear_actuator, elbow, wrist_left, wrist_right]
+
 while True:
+
+    # Update the xbox controller events
     sdl2.SDL_PumpEvents()
 
+    # Grabs data from both joysticks and stores them in arrays
     arm_data = []
-    for i in range(0, 6):
-        arm_data.append(sdl2.SDL_JoystickGetAxis(arm_joy, i))
-    for i in range(0, 11):
-        arm_data.append(sdl2.SDL_JoystickGetButton(arm_joy, i))
-    arm_data.append(sdl2.SDL_JoystickGetHat(arm_joy, 0))
-
     drive_data = []
     for i in range(0, 6):
+        arm_data.append(sdl2.SDL_JoystickGetAxis(arm_joy, i))
         drive_data.append(sdl2.SDL_JoystickGetAxis(drive_joy, i))
     for i in range(0, 11):
+        arm_data.append(sdl2.SDL_JoystickGetButton(arm_joy, i))
         drive_data.append(sdl2.SDL_JoystickGetButton(drive_joy, i))
+    arm_data.append(sdl2.SDL_JoystickGetHat(arm_joy, 0))
     drive_data.append(sdl2.SDL_JoystickGetHat(drive_joy, 0))
-
-    if ((arm_data[14] or drive_data[14]) and not pressed14):
-        pressed14 = True
-    if (not arm_data[14] and not drive_data[14] and pressed14):
-        pressed14 = False
-        arm_joy, drive_joy = drive_joy, arm_joy
-
-    arm_msg = json.dumps(arm_data)
-    node.send("arm", arm_msg)
-    drive_msg = json.dumps(drive_data)
-    node.send("drive", drive_msg)
-
-    time.sleep(0.01)
-
     # Data array format:
     ## 16 bit signed ints:
     # 0  | right joystick x
@@ -67,3 +62,20 @@ while True:
     ## 4 bit
     # 17 | hat switch / dpad
     #    | (1 = N, 3 = NE, 2 = E, 6 = SE, 4 = S, 12 = SW, 8 = W, 9 = NW)
+
+    # Handles switching xbox controllers by clicking the xbox button (14)
+    if ((arm_data[14] or drive_data[14]) and not pressed14):
+        pressed14 = True
+    if (not arm_data[14] and not drive_data[14] and pressed14):
+        pressed14 = False
+        arm_joy, drive_joy = drive_joy, arm_joy
+
+    # Actually sends the data
+    arm_msg = json.dumps(handle_arm_input(arm_data))
+    drive_msg = json.dumps(drive_data)
+    node.send("arm", arm_msg)
+    node.send("drive", drive_msg)
+
+    # Small time delay so nothing freaks out
+    time.sleep(0.01)
+
